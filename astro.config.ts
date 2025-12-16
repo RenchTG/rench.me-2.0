8860831f74286ff2afeddd44afaaff49e368b716
Philip Dobranowski <dobranowskip1@gmail.com>
2025-12-15 22:25:57 -0700

    Oops it cant be redirect url

--- astro.config.ts
+++ astro.config.ts
@@ -20,7 +20,7 @@ import icon from 'astro-icon'
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://rench.me',
+  site: 'https://philipdobranowski.com',
   integrations: [
     tailwind({
       applyBaseStyles: false,
